58f0801896b9c606ff8daf8cf837a0cab2a4ccc3
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-06-16 19:56:24 +0200

    Add footer

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Hero } from "./Hero";
 
@@ -6,6 +7,7 @@ export function Homepage() {
     <>
       <Header />
       <Hero />
+      <Footer />
     </>
   );
 }
